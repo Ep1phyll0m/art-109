e35c10ffc62240fc93e9965ac192b1c291cf9cca
Ep1phyll0m <lg.quynh.al@gmail.com>
2026-02-28 14:23:37 -0800

    correction Tut8

--- tutorials/2-css/06-css-portfolio-grid/js/hamburger.js
+++ tutorials/2-css/06-css-portfolio-grid/js/hamburger.js
@@ -4,7 +4,7 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.nav-menu');
 
 hamburger.addEventListener('click', () => {
-    // menu.classList.toggle('active');
+    menu.classList.toggle('active');
     console.log("clicked");
 })
 
