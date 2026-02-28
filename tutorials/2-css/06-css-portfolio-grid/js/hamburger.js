2276d67ad9018a3b67074033fdcbda4ad44c480b
Ep1phyll0m <lg.quynh.al@gmail.com>
2026-02-27 18:09:02 -0800

    Tut8.1

---
+++ tutorials/2-css/06-css-portfolio-grid/js/hamburger.js
@@ -0,0 +1,10 @@
+// console.log("I'm here!");
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    // menu.classList.toggle('active');
+    console.log("clicked");
+})
+
