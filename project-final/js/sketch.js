ec60a34c1730417ec5621fe50e8e8d89d1eeee8b
sprymint <dazzlingjupiter@gmail.com>
2026-05-17 23:08:04 -0700

    cursor_images

---
+++ project-final/js/sketch.js
@@ -0,0 +1,56 @@
+let xPos = 0 //stores lagging x position
+let yPos = 0 //stores lagging y position
+let easing = 0.05; //speed of ease 
+let canvas;
+
+function preload() {
+    img = loadImage('images/ken_cursor.png');
+}
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style("z-index", -2);
+    describe('An isolated image of one of my cats');
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    //background(125);
+    clear();
+    xPos = xPos + ((mouseX - xPos) * easing)
+    yPos = yPos + ((mouseY - yPos) * easing)
+
+    ////drawThing(xPos, yPos);
+    drawTbh(xPos, yPos);
+}
+
+function mouseMoved() {
+
+    //drawThing(mouseX - 50, mouseY + 75);
+}
+
+function drawThing(_x, _y) {
+
+    //draw eyeballs
+    // fill(255);
+    // ellipse(_x, _y, 30, 30);
+    // ellipse(_x - 20, _y, + 5, 30, 30);
+
+
+    // //draw pupils
+    // fill(0);
+    // ellipse(_x + 10, _y, 5, 5);
+    // ellipse(_x - 10, _y + 5, 5, 5);
+
+    // strokeWeight(0);
+    // fill(random(200, 255), random(200, 255), random(200, 255));
+    // ellipse(_x, _y, 30, 30);
+}
+
+function drawCat(_x, _y) {
+    image(img, _x + 10, _y, 90, 100, 5);
+}
